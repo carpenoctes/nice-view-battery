35e76ea6f01195ae634a817473e8136d7eaac0f1
carpenoctes <114108233+carpenoctes@users.noreply.github.com>
2025-06-03 09:18:28 +0200

    Update layer.c

--- boards/shields/nice_view_battery/widgets/layer.c
+++ boards/shields/nice_view_battery/widgets/layer.c
@@ -15,7 +15,7 @@ void draw_layer_status(lv_obj_t *canvas, const struct status_state *state) {
 
     char text[10] = {};
     if (state->layer_label == NULL || strlen(state->layer_label) == 0) {
-        sprintf(text, "LAYER %d", "1");
+        sprintf(text, "LAYER %d", 1);
     }
     else{
         strcpy(text, state->layer_label);
@@ -23,5 +23,5 @@ void draw_layer_status(lv_obj_t *canvas, const struct status_state *state) {
     }
 
     lv_canvas_draw_img(canvas, OFFSET_X, OFFSET_Y, &layer, &img_dsc);
-    lv_canvas_draw_text(canvas, OFFSET_X + 2, OFFSET_Y, 64, &label_dsc, text);
+    lv_canvas_draw_text(canvas, OFFSET_X + 2, OFFSET_Y, 68, &label_dsc, text);
 }
